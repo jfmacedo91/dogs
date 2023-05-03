1fc4f5e3325f2b939f70be7393b54f4cd21814e8
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2023-05-03 06:36:57 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: 'detect' } },
+  plugins: ['react-refresh', 'react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unsafe-finally': 'off',
+    'no-unused-vars': 'off',
+    'react-refresh/only-export-components': 'off'
+  },
+}
